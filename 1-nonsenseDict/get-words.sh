e3d77fa8c35d821f9855c6cd10b4e668e6add571
Camille Goudeseune <cog@illinois.edu>
2017-07-05 11:50:20 -0500

    comments

--- 1-nonsenseDict/get-words.sh
+++ 1-nonsenseDict/get-words.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 #
 # Extract nonsense words from a too-big pronunciation dictionary, whose lines have the form
-# a'laahamaschi a l a ah a m a sc h i
-# a'laahamaschi a l aa h a m a s c h i
+#     a'laahamaschi a l a ah a m a sc h i
+#     a'laahamaschi a l aa h a m a s c h i
+#		(from dict_grapheme.txt)
+# or
+#     alaamgili 0.14373  a l aa m gi l i
+#     alaamgili 0.05287 al a a m g i l i
+#		(from normal_dict_prob.txt)
 #
-# Usage: ./get-words.sh < dict_grapheme.txt > dict_words.txt
+# Usage: ./get-words.sh < in.txt > dict_words.txt
 
 # This gets the first word of each input line,
-# and removes duplicates due to multiple pronunciations per word.
+# and removes duplicates that are due to multiple pronunciations per word.
 
 # The rest of this pipeline typically reduces 331k words to 328k words:
 #
