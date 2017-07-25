c833f43758eae6ef2bb6893715ef8ad5596dece0
Camille Goudeseune <cog@illinois.edu>
2017-07-25 14:52:50 -0500

    cosmetics

--- s5c/local/ldc_data_prep.sh
+++ s5c/local/ldc_data_prep.sh
@@ -8,36 +8,31 @@ pref='' # 'UZB'
 odir='data/$lang/data'
 
 if [ "$#" -lt 3 ]; then
-  echo "Usage: $0 <src-dir> <mc-transcript-dir> <dst-dir>"
-  echo "e.g.: $0 Uzbek/LDC2016E66/UZB_20160711 fromCamille/leda-uzbek data/LDC2016E66"
+  echo "Usage: $0 srcDir MCTranscriptDir dstDir [lang-prefix]"
+  echo "e.g.: $0 Uzbek/LDC2016E66/UZB_20160711 fromCamille/leda-uzbek data/LDC2016E66 UZB"
   exit 1
 fi
 if [ "$#" -eq 4 ]; then
   pref=$4
 fi 
 
-# Use DATA_ROOT.txt file for basedir for Audio data
+# Base dir for audio data.
 basedatadir=$(cat DATA_ROOT.txt)
-echo $basedatadir
 
 src=$basedatadir/$1
 transcriptdir=$2
 dst=$3
 
-# export LC_ALL=C
 export LC_ALL=en_US.UTF-8
 
 mkdir -p $dst
-
 for d in $src/*/AUDIO; do
-    ls $d/*.flac 
+  ls $d/*.flac 
 done > $dst/all.flist
 
 fs=44100
 nbits=16
 sox_options="-t flac -r $fs -e signed-integer -b $nbits"
-
-which sox
 
 wavdir=$dst/wav
 olist=$dst/wav.list
@@ -46,33 +41,30 @@ wavscp=$dst/wav.scp
 mkdir -p $wavdir
 
 if [ ! -e $wavscp ]; then
-while read line; do 
+  while read line; do 
     bname=$(basename $line)
     bname=${bname%.flac}
-    # echo $bname
     sox $sox_options $line -t wav $wavdir/${bname}.wav
 
     nsamples=`soxi -s "$wavdir/${bname}.wav"`;
     # Check if the file is too short, less than 1000 samples
     if [[ "$nsamples" -gt 1000 ]]; then 
-	echo "$wavdir/${bname}.wav" >> $olist;
-	echo "$bname $wavdir/${bname}.wav" >> $wavscp
+      echo "$wavdir/${bname}.wav" >> $olist;
+      echo "$bname $wavdir/${bname}.wav" >> $wavscp
     fi
-done < $dst/all.flist
+  done < $dst/all.flist
 fi
 
-echo "./local/generate_data.py $transcriptdir  $dst/segments $dst/vocab $dst/text \
-    --utt_prefix $pref;"
-./local/generate_data.py $transcriptdir $dst/segments $dst/vocab $dst/text \
-    --utt_prefix $pref;
+echo "local/generate_data.py $transcriptdir $dst/segments $dst/vocab $dst/text --utt_prefix $pref;"
+local/generate_data.py $transcriptdir $dst/segments $dst/vocab $dst/text --utt_prefix $pref;
 # utt2spk? 
 paste <(cut -d ' ' -f 1 $dst/text) <(cut -d '_' -f 1,2,3 $dst/text ) > $dst/utt2spk
-# # Since we do not have speaker information, utt2spk maps utt to utt
+# Because we lack speaker information, utt2spk maps utt to utt.
 utils/utt2spk_to_spk2utt.pl $dst/utt2spk > $dst/spk2utt 
 
-cat $dst/vocab | tr ' ' '\n'  | grep -v '<UNK>' > $dst/vocab.words
+tr ' ' '\n' < $dst/vocab | grep -v '<UNK>' > $dst/vocab.words
 
-# Files may not be sorted, fix it 
+# Fix any unsorted files.
 export LC_ALL=C
 utils/data/fix_data_dir.sh $dst
 
