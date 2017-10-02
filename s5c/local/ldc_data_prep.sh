81a3a82966688e1783fafd4a0a0fb8ab0ed63f4e
Camille Goudeseune <cog@illinois.edu>
2017-10-02 16:49:02 -0500

    read .wav's if there's no .flac's

--- s5c/local/ldc_data_prep.sh
+++ s5c/local/ldc_data_prep.sh
@@ -4,32 +4,43 @@ export LC_ALL=en_US.UTF-8
 
 # todo: If $MCTranscriptdir has no *.txt, abort!  Maybe the scrips are accidentally in a subdir.
 
-[ -d $data/$lang_subdir ] || { echo "Missing data/lang_subdir directory of .flac files '$data/$lang_subdir'. Check the settings file."; exit 1; }
+[ -d $data/$lang_subdir ] || { echo "Missing data/lang_subdir directory of audio files '$data/$lang_subdir'. Check the settings file. Aborting."; exit 1; }
 
 dst=data/$lang
 mkdir -p $dst
-find $data/$lang_subdir -type f -name \*.flac > $dst/all.flist
+flist=$dst/all.flist
+audioformat=flac
+find $data/$lang_subdir -type f -name \*.$audioformat > $flist
+if [[ ! -s $flist ]]; then
+  # Found no .flac files.  Try .wav instead.
+  audioformat=wav
+  find $data/$lang_subdir -type f -name \*.$audioformat > $flist
+fi
+if [[ ! -s $flist ]]; then
+  echo "No .flac or .wav files in '$data/$lang_subdir'. Aborting."
+  rm $flist
+  exit 1
+fi
 wavdir=$dst/wav
 mkdir -p $wavdir
 
 nbits=16
-sox_options="-t flac -r $sample_rate -e signed-integer -b $nbits"
+sox_options="-t $audioformat -r $sample_rate -e signed-integer -b $nbits"
 olist=$dst/wav.list
 wavscp=$dst/wav.scp
-if [ ! -e $wavscp ]; then
-  while read line; do 
-    bname=$(basename $line)
-    bname=${bname%.flac}
-    sox $sox_options $line -t wav $wavdir/${bname}.wav
+# Rebuild $wavdir, $wavscp, and $olist from $flist, even if those already exist.
+while read line; do 
+  bname=$(basename $line)
+  bname=${bname%.$audioformat}
+  sox $sox_options $line -t wav $wavdir/${bname}.wav
 
-    # Skip any file shorter than 1000 samples.
-    nsamples=`soxi -s "$wavdir/${bname}.wav"`
-    if [[ "$nsamples" -gt 1000 ]]; then 
-      echo "$wavdir/${bname}.wav" >> $olist
-      echo "$bname $wavdir/${bname}.wav" >> $wavscp
-    fi
-  done < $dst/all.flist
-fi
+  # Skip any file shorter than 1000 samples.
+  nsamples=`soxi -s "$wavdir/${bname}.wav"`
+  if [[ "$nsamples" -gt 1000 ]]; then 
+    echo "$wavdir/${bname}.wav" >> $olist
+    echo "$bname $wavdir/${bname}.wav" >> $wavscp
+  fi
+done < $flist
 
 if [ -z "$scrip_timing_in_samples" ]; then
   # Transcription timings are in microseconds.
