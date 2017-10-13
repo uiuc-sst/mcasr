fb8cdcc2c98fd3f67c8a258a85b49b636501fc8b
Camille Goudeseune <cog@illinois.edu>
2017-10-13 15:42:44 -0500

    diagnostics

--- s5c/local/g2p.sh
+++ s5c/local/g2p.sh
@@ -11,11 +11,10 @@
 
 if [ $# -lt 3 ]; then
   echo "Usage: $0 <vocab> <g2p-model-dir> <out-lexicon> [model-order [num-variants]]"
+  echo "  <vocab>: words for which to generate pronunciations, e.g. data/local/dict/g2p/vocab_autogen"
+  echo "  <g2p-model-dir>: directory containing the G2P model"
+  echo "  <out-lexicon>: the generated pronunciations, e.g. data/\$lang/g2p/lexicon_autogen.1"
   echo "e.g.: $0 data/local/dict/g2p/vocab_autogen.1 /export/a15/vpanayotov/data/g2p data/local/dict/g2p/lexicon_autogen.1"
-  echo ", where:"
-  echo "    <vocab> - input vocabulary, that's words for which we want to generate pronunciations"
-  echo "    <g2p-model-dir> - source directory where g2p model is located"
-  echo "    <out-lexicon> - the output, i.e. the generated pronunciations"
   exit 1
 fi
 
@@ -23,7 +22,7 @@ vocab=$1
 g2p_model_dir=$2
 out_lexicon=$3
 
-[ ! -f $vocab ] && echo "Can't find the G2P input file: $vocab" && exit 1;
+[ ! -f $vocab ] && echo "Missing G2P input file '$vocab'." && exit 1
 
 if [ $# -ge 4 ]; then
     model_order=$4
@@ -40,21 +39,17 @@ sequitur_model=$g2p_model_dir/model-$model_order
 echo "G2P model order: $model_order"
 echo "Number of variants to generate: $num_variants"
 
-# Turns out, that Sequitur has some sort of bug so it doesn't output pronunciations
-# for some (admittedly peculiar) words. We manually specify these exceptions below
-g2p_exceptions="HH HH" # more such entries can be added, separated by "\n"
+[ ! -f  $sequitur ] && echo "Missing Sequitur G2P.  See $KALDI_ROOT/tools." && exit 1
+[ ! -d $sequitur_path ] && echo "Missing '$sequitur_path'.  Fix the Sequitur installation." && exit 1
+[ ! -f $sequitur_model ] && echo "Missing Sequitur model file '$sequitur_model'." && exit 1
 
-[ ! -f  $sequitur ] && \
-  echo "Can't find the Sequitur G2P script. Please check $KALDI_ROOT/tools for installation script and instructions" && \
-  exit 1;
-
-[ ! -d $sequitur_path ] && echo "Can't find '$sequitur_path' - please fix your Sequitur installation" && exit 1
-[ ! -f $sequitur_model ] && echo "Can't find the Sequitur model file: $sequitur_model" && exit 1
+# Sequitur incorrectly fails to output pronunciations for some (peculiar) words.
+# List these exceptions here, delimited by \n.
+g2p_exceptions="HH HH"
 
 PYTHONPATH=$sequitur_path:$PYTHONPATH $PYTHON $sequitur \
   --model=$sequitur_model --apply $vocab --variants-number $num_variants \
   >${out_lexicon}.tmp || exit 1
-
 
 awk 'NR==FNR{p[$1]=$0; next;} {if ($1 in p) print p[$1]; else print}' \
   <(echo -e $g2p_exceptions) ${out_lexicon}.tmp >$out_lexicon || exit 1
