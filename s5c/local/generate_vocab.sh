de85a32db20dff6ff5f8482301b0f528b3b6d14a
Camille Goudeseune <cog@illinois.edu>
2017-11-17 16:15:11 -0600

    error handling

--- s5c/local/generate_vocab.sh
+++ s5c/local/generate_vocab.sh
@@ -15,24 +15,22 @@ pron_variants=5
 . ./path.sh || exit 1
 . utils/parse_options.sh || exit 1
 
-if [ "$#" -ne 4 ]; then
-  >&2 echo "Usage: $0 G2P_model_dir phone_set G2P_data_dir < MC_vocab/dict > logfile"
-  exit 1
-fi
+[ "$#" -ne 3 ] && >&2 echo "Usage: $0 G2P_model_dir phone_set G2P_data_dir < MC_vocab/dict &> logfile" && exit 1
+# e.g., inputs/g2p_reduced_model inputs/phoneset.txt data/myLanguage/g2p
  
 g2pmodeldir=$1
 phoneset=$2
 dd=$3 # Data dir.
 
-[ ! -d $g2pmodeldir ] && >&2 echo "$0: missing G2P model directory $g2pmodeldir." && exit 1
+[ ! -d $g2pmodeldir ] && >&2 echo "$0: missing G2P model dir $g2pmodeldir." && exit 1
+[ ! -f $phoneset    ] && >&2 echo "$0: missing phone set $phoneset." && exit 1
 
 >&2 echo "$0: model order and number of pronunciation variants for G2P application are: $model_order $pron_variants"
 
 # Copy the g2p model and the phone set to the data dir.
-mkdir -p $dd
 g2pmodel=$g2pmodeldir/model-$model_order
-[ ! -f $g2pmodel ] && >&2 echo "$0: missing G2P model file $g2pmodel." && exit 1
-[ ! -f $phoneset ] && >&2 echo "$0: missing phone set file $phoneset." && exit 1
+[ ! -f $g2pmodel ] && >&2 echo "$0: missing G2P model $g2pmodel." && exit 1
+mkdir -p $dd
 cp $g2pmodel $phoneset $dd
 
 # Back up any vocab.plain from a previous run of this script (called by local/apply_g2p.sh).
